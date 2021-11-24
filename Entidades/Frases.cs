c7034907794b40f35594f0e59259f1447c1c4c69
Rodrigo Gimenes Raffi <rodrigogimenes0021@gmail.com>
2021-11-23 21:26:18 -0300

    Base do programa feita

---
+++ Entidades/Frases.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeRace.NovaPasta
+{
+    class Frases
+    {
+        public string Frase;
+        private string[] lines = System.IO.File.ReadAllLines(@"C:\Users\User\source\repos\TypeRace\TypeRace\Frases.txt");
+        public int NumeroDeLetras;
+
+        public Boolean Correcao(string frase)
+        {
+            if (frase == Frase)
+            {
+                return true;
+            } else
+            return false;
+        }
+        public string FraseAleatoria()
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(0,lines.Length);
+            return lines[num];
+        }
+    }
+}
